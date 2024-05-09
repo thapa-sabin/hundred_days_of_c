6d00c244390a5590ec8c5d580010b60ec5a59954
Sabin Thapa <thapasabin@zohomail.com>
2024-05-09 21:23:24 +0545

    Added the functionality to reverse a string

--- string_reverse/reverse_string.c
+++ string_reverse/reverse_string.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+void reverse_string(char *from, char *to) {
+    int offset = 0;
+    int length = strlen(from) - 1;
+    while (from[offset] != 0) {
+        to[offset] = from[length];
+        offset++;
+        length--;
+    }
+    to[offset] = 0; // Making it NULL terminated
+}
+
 // Strings I am working with
 int main(int argc, char **argv) {
     char str[] = "Hello";
